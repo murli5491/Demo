9a72f392fd4b8408a6e777df0707b854f2a98f51
murli <murlisist87@gmail.com>
2025-08-17 18:29:28 +0530

    today commit

---
+++ features/demo.feature
@@ -0,0 +1,7 @@
+Feature: orange CRM
+  Scenario: login the application
+    Given launch the browser
+    When open the orange HRM
+    And Enter UserName "Admin" And EnterPassword "admin123"
+    And click on login button
+    Then user must successfully login to the dashboard page
---
+++ features/steps/login.py
@@ -0,0 +1,26 @@
+from behave import given, when, then
+
+
+@given(u'launch the browser')
+def step_impl(context):
+    pass
+
+
+@when(u'open the orange HRM')
+def step_impl(context):
+    pass
+
+
+@when(u'Enter UserName "Admin" And EnterPassword "admin123"')
+def step_impl(context):
+    pass
+
+
+@when(u'click on login button')
+def step_impl(context):
+    pass
+
+
+@then(u'user must successfully login to the dashboard page')
+def step_impl(context):
+    pass
