b6656c1be5a59ec1e381cc1d8e0b0bf9c990f29f
murli5491 <murlivishwa5491@gmail.com>
2025-08-18 12:59:52 +0530

    Delete features/envirorment.py

--- features/envirorment.py
+++
@@ -1,6 +0,0 @@
-
-
-def before_all(context):
-    print("welcome ")
-    context.driver.maximize_window()
-    context.driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
